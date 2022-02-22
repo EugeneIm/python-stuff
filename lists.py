c82b57b38e68ab4a6f0fa532b6d067257a4de1f2
EugeneIm <immer277@gmail.com>
2022-02-22 14:58:08 -0800

    starting arrays

---
+++ lists.py
@@ -0,0 +1,11 @@
+#working with lists in python
+characters = ['willy wonka', 'donald duck', 'ditto', 'nessa']
+for character in characters: 
+	#print(character) 
+		#the above just lists out the array
+	print(character.title() + " is a great character")
+		#the above lists the characters along with a string. 
+#using a for loop to go through the list of characters. 
+#if you add a separate print statement after the for loop, it prints that message after the for loop ends
+
+print("thank you for coming everyone")
