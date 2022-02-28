7cb7c9a27beb63f7a925a056c87def0b9b74973f
EugeneIm <immer277@gmail.com>
2022-02-28 15:32:56 -0800

    added to the lists file

--- lists.py
+++ lists.py
@@ -2,6 +2,15 @@
 car_brands = ['Tesla', 'Audi', 'Hona', 'BMW', 'Mercedes']
 print(car_brands)
 
-#accessing certian items in the list 
+# #accessing certian items in the list 
 print(car_brands[1])
-#output = Audi.
+# #output = Audi.
+
+print(car_brands[0].title)
+#This prints the car brand titled, you can also do ".upper" or ".lower"
+
+car_brands.append('Volkswagen')
+print(car_brands)
+car_brands.insert(0,"Ferrari")
+#This inserts "Ferrari" at the beginning of the list
+
